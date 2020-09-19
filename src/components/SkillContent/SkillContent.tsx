0b09bd41e4b327ef39fd9c90d12d1b0d9f62ecd1
snapCheol <chief1689@naver.com>
2020-09-19 21:32:33 +0900

    스킬 아이콘 추가

--- src/components/SkillContent/SkillContent.tsx
+++ src/components/SkillContent/SkillContent.tsx
@@ -1,23 +1,103 @@
 import React from 'react';
+import {
+  SiHtml5,
+  SiCss3,
+  SiJquery,
+  SiJavascript,
+  SiSass,
+  SiReact,
+  SiRedux,
+  SiTypescript,
+  SiGit,
+} from 'react-icons/si';
+import styled from 'styled-components';
 import ContentLayout from '../ContentLayout/ContentLayout';
+
+const SkillList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+
+  & > li {
+    flex-grow: 1;
+    width: 33.3333%;
+    text-align: center;
+    padding: 5%;
+    & * {
+      display: block;
+    }
+    & .skill-icon {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 5px;
+      font-size: 40px;
+    }
+    & strong {
+      font-weight: 300;
+    }
+  }
+`;
 
 const SkillContent = () => {
   return (
     <ContentLayout>
       <h3 lang="en">Skills</h3>
 
-      <ul>
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>jQUery</li>
-        <li>Javascript</li>
-        <li>ES6</li>
-        <li>SCSS</li>
-        <li>REACT</li>
-        <li>REDUX</li>
-        <li>REDUX-SAGA</li>
-        <li>TYPESCRIPT</li>
-      </ul>
+      <SkillList>
+        <li>
+          <span className="skill-icon">
+            <SiHtml5 />
+          </span>
+          <strong>HTML5</strong>
+        </li>
+        <li>
+          <span className="skill-icon">
+            <SiCss3 />
+          </span>
+          <strong>CSS3</strong>
+        </li>
+        <li>
+          <span className="skill-icon">
+            <SiJquery />
+          </span>
+          <strong>jQuery</strong>
+        </li>
+        <li>
+          <span className="skill-icon">
+            <SiJavascript />
+          </span>
+          <strong>Javascript</strong>
+        </li>
+        <li>
+          <span className="skill-icon">
+            <SiSass />
+          </span>
+          <strong>SCSS</strong>
+        </li>
+        <li>
+          <span className="skill-icon">
+            <SiReact />
+          </span>
+          <strong>REACT</strong>
+        </li>
+        <li>
+          <span className="skill-icon">
+            <SiRedux />
+          </span>
+          <strong>Redux</strong>
+        </li>
+        <li>
+          <span className="skill-icon">
+            <SiTypescript />
+          </span>
+          <strong>Typescript</strong>
+        </li>
+        <li>
+          <span className="skill-icon">
+            <SiGit />
+          </span>
+          <strong>Git</strong>
+        </li>
+      </SkillList>
     </ContentLayout>
   );
 };
