9ef2204a6634badeb2035f695f3fe125b8439075
snapCheol <chief1689@naver.com>
2020-09-28 09:58:33 +0900

    Updates

---
+++ precache-manifest.fb1e314b7aef051abd93246627ac5ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5123196797bab0cced81c6f8bea39c",
+    "url": "/Blog/index.html"
+  },
+  {
+    "revision": "9ccc9bf38971cc1f6377",
+    "url": "/Blog/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "9ccc9bf38971cc1f6377",
+    "url": "/Blog/static/js/2.45c21437.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Blog/static/js/2.45c21437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8242c7114400693e7275",
+    "url": "/Blog/static/js/main.3f4216ee.chunk.js"
+  },
+  {
+    "revision": "91d1493bb562e6483085",
+    "url": "/Blog/static/js/runtime-main.ee659c6e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.6d3baa9346869f02a8bed7e929259acd.js"
+  "/Blog/precache-manifest.fb1e314b7aef051abd93246627ac5ba8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
